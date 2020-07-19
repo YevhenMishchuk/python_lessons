9c040e250863b7e17d2cfc1ab703de71df21eb2e
YevhenMishchuk <eomarseille@gmail.com>
2020-07-19 23:11:48 +0300

    functional programming

---
+++ exDecor.py
@@ -0,0 +1,12 @@
+def decor(func):
+  def wrap():
+    print("============")
+    func()
+    print("============")
+  return wrap
+
+def print_text():
+  print("Hello world!")
+
+decorated = decor(print_text)
+decorated()
---
+++ exLambda.py
@@ -0,0 +1,2 @@
+
+print((lambda x: x*2+x**3) (2))
